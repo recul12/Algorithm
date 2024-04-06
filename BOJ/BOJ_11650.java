7dee5a54e76387de9f607c4d5e9698b1ed52b849
Heeyeon Jeong <recul12@naver.com>
2024-04-06 23:35:40 +0900

    add BOJ_11650
    BOJ_11650 좌표 정렬하기

---
+++ BOJ/BOJ_11650.java
@@ -0,0 +1,36 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11650 {
+    /*
+        2차원 평면 위에 점 N개가 주어진다.
+        좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하라.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int array[][] = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array, ((o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0]-o2[0]));
+
+        for (int i = 0; i < N; i++) {
+            sb.append(array[i][0]).append(" ").append(array[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
